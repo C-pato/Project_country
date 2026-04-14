9cef49386bb7fe7a83ccdad076edcab2bac41531
larsdubbink <dubbinklars@gmail.com>
2026-04-14 11:35:32 +0200

    commit 2

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require("express")
 
 const app = express()
-
+app.use(express.json())
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    try {
+        fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+        .then(data => {
+            return data.json()
+        })
+        .then(json => {
+            res.json(json)
+        })
+    } catch (error) {
+        res.send(error)
+    }
+    
 })
 
 app.listen(3000, () => {
