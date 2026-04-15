8225808c691bb00d4ee1456e77f4b5d9c9e0fa90
larsdubbink <dubbinklars@gmail.com>
2026-04-15 11:13:12 +0200

    added a /scores to post new scores, and a /show and /leaderboard to /scores

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
 const express = require("express")
+const fs = require("fs")
+
+function loadUsers() {
+    const data = fs.readFileSync("scores.json", "utf-8")
+    return JSON.parse(data)
+}
+
+function saveUsers(users) {
+    fs.writeFileSync("scores.json", JSON.stringify(users, null, 2))
+}
 
 async function randomCountries(randomCountriesAmount) {
     return fetch("http://localhost:3000/country-data")
@@ -136,6 +146,48 @@ app.get("/country-data", (req, res) => {
         res.send(error)
     }
     
+})
+
+app.post("/scores", (req, res) => {
+    const name = req.body.name
+    const score = req.body.score
+    
+    const users = loadUsers()
+
+    let user = users.find(u => u.name == name)
+
+    if (!user) {
+        user = {name, scores: []}
+        users.push(user)
+    }
+
+    user.scores.push(score)
+    saveUsers(users)
+    res.send(user)
+})
+
+app.get("/scores/show", (req, res) => {
+    const users = loadUsers()
+
+    res.json(users)
+})
+
+app.get("/scores/leaderboard", (req, res) => {
+    const users = loadUsers()
+
+    let allscores = []
+
+    users.forEach(user => {
+        user.scores.forEach(score => {
+            allscores.push({
+                user: user.name,
+                score: score
+            })
+        });
+    });
+    allscores.sort((a, b) => b.score - a.score)
+
+    res.json(allscores.slice(0, 10))
 })
 
 app.listen(3000, () => {
